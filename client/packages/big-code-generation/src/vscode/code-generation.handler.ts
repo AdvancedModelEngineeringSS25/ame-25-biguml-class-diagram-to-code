c32bc46a89f1c49478524789e8ef56f70333f758
Lukas Karafiat <lukas.karafiat@gmx.at>
2025-05-06 12:12:01 +0200

    added experiment with java code generation

--- client/packages/big-code-generation/src/vscode/code-generation.handler.ts
+++ client/packages/big-code-generation/src/vscode/code-generation.handler.ts
@@ -19,6 +19,19 @@ import { DisposableCollection } from '@eclipse-glsp/protocol';
 import { inject, injectable, postConstruct } from 'inversify';
 import { CodeGenerationActionResponse, RequestCodeGenerationAction } from '../common/code-generation.action.js';
 
+import {
+    CSharpTargetLanguage,
+    FetchingJSONSchemaStore,
+    getTargetLanguage,
+    InputData,
+    JavaTargetLanguage,
+    JSONSchemaInput,
+    PythonTargetLanguage,
+    quicktype,
+    TypeScriptTargetLanguage,
+    type TargetLanguage
+} from 'quicktype-core';
+
 // Handle the action within the server and not the glsp client / server
 @injectable()
 export class CodeGenerationActionHandler implements Disposable {
@@ -41,6 +54,29 @@ export class CodeGenerationActionHandler implements Disposable {
                 if (model) {
                     const sourceModel = model.getSourceModel();
                     console.log('Model available', sourceModel);
+
+                    const personSchema = {
+                        $schema: 'http://json-schema.org/draft-07/schema#',
+                        type: 'object',
+                        properties: {
+                            author: {
+                                type: 'object',
+                                properties: {
+                                    name: { type: 'string' },
+                                    preferredName: { type: 'string' },
+                                    age: { type: 'number' },
+                                    gender: { enum: ['male', 'female', 'other'] }
+                                },
+                                required: ['name', 'preferredName', 'age', 'gender']
+                            },
+                            title: { type: 'string' },
+                            publisher: { type: 'string' }
+                        },
+                        required: ['author', 'title', 'publisher']
+                    };
+
+                    const { lines: javaPerson } = await this.quickTypeJSONSchema('java', 'Person', JSON.stringify(personSchema));
+                    console.log(javaPerson);
                 } else {
                     console.log('No model available');
                 }
@@ -54,5 +90,42 @@ export class CodeGenerationActionHandler implements Disposable {
 
     dispose(): void {
         this.toDispose.dispose();
+    }
+
+    private getTargetLanguageInstance(lang: string): TargetLanguage {
+        switch (lang.toLowerCase()) {
+            case 'java':
+                return new JavaTargetLanguage();
+            case 'typescript':
+                return new TypeScriptTargetLanguage();
+            case 'csharp':
+                return new CSharpTargetLanguage();
+            case 'python':
+                return new PythonTargetLanguage();
+            default:
+                throw new Error(`Unsupported language: ${lang}`);
+        }
+    }
+
+    private async quickTypeJSONSchema(targetLanguage: string, typeName: string, jsonSchemaString: string) {
+        const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
+
+        await schemaInput.addSource({ name: typeName, schema: jsonSchemaString });
+
+        const inputData = new InputData();
+        inputData.addInput(schemaInput);
+
+        const lang = getTargetLanguage(this.getTargetLanguageInstance(targetLanguage));
+        if (lang === undefined) {
+            throw new Error(`Unsupported language: ${targetLanguage}`);
+        }
+
+        return await quicktype({
+            inputData,
+            lang: lang,
+            rendererOptions: {
+                'just-types': true
+            }
+        });
     }
 }
